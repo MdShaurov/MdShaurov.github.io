fe4236c73f8a7115acf04d52a2805ea9f954042b
MdShaurov <shaurovmd123@gmail.com>
2021-10-14 12:11:00 -0600

    game of life started

---
+++ game of life/sketch.js
@@ -0,0 +1,26 @@
+// Game of Life
+
+let grid;
+let gridSize = 30;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = create2DArray(gridSize, gridSize);
+}
+
+function draw() {
+  background(220);
+  
+  displayGrid();
+}
+
+function displayGrid() {
+
+}
+
+function createRandomGrid(rows, cols) {
+  let x = [];
+  for (let i=0;i<cols; i++) {
+  }
+}
