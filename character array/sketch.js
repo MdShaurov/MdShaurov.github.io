a36e0dd274445c73dfb5e04e10fb3787b4f0b5d4
MdShaurov <shaurovmd123@gmail.com>
2021-10-18 11:58:13 -0600

    Gosper gun and character demo

---
+++ character array/sketch.js
@@ -0,0 +1,94 @@
+// Character In Grid
+
+let grid;
+let gridSize = 20;
+let cellWidth, cellHeight;
+let level;
+let playerX;
+let playerY;
+
+function preload() {
+  level = loadJSON("assets/level1.json"); //assumes gridSize is 20
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = level;
+  cellWidth = width/gridSize;
+  cellHeight = height/gridSize;
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function keyPressed() {
+  if (key === "e") {
+    grid = createEmpty2DArray(gridSize, gridSize);
+  }
+  if (key === "r") {
+    grid = createRandom2DArray(gridSize, gridSize);
+  }
+  if (key === "s") {
+    tryToMoveTo(playerX, playerY+1);
+  }
+}
+
+function tryToMoveTo(newX, newY) {
+  playerX = newX;
+  playerY = newY;
+  grid[playerY][playerX] = 9;
+}
+function mousePressed() {
+  let cellX = Math.floor(mouseX/cellWidth);
+  let cellY = Math.floor(mouseY/cellHeight);
+
+  if (grid[cellY][cellX] === 0) {
+    grid[cellY][cellX] = 1;
+  }
+  else if (grid[cellY][cellX] === 1) {
+    grid[cellY][cellX] = 0;
+  }
+}
+
+function displayGrid() {
+  for (let y=0; y<gridSize; y++) {
+    for (let x=0; x<gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
+
+function createEmpty2DArray(rows, cols) {
+  let board = [];
+  for (let y=0; y<rows; y++) {
+    board.push([]);
+    for (let x=0; x<cols; x++) {
+      board[y].push(0);
+    }
+  }
+  return board;
+}
+
+function createRandom2DArray(rows, cols) {
+  let board = [];
+  for (let y=0; y<rows; y++) {
+    board.push([]);
+    for (let x=0; x<cols; x++) {
+      if (random(100) < 50) {
+        board[y].push(0);
+      }
+      else {
+        board[y].push(1);
+      }
+    }
+  }
+  return board;
+}
--- game of life/sketch.js
+++ game of life/sketch.js
@@ -1,32 +1,33 @@
 // Game of Life
 
 let grid;
-let gridSize = 30;
+let gridSize = 60;
 let cellWidth, cellHeight;
-let autoplay = false;
+let autoPlay = false;
+let gun;
+
+function preload() {
+  gun = loadJSON("assets/gosper-gun.json"); //assumes gridsize is 60
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grid = createRandom2DArray(gridSize, gridSize);
+  grid = createEmpty2DArray(gridSize, gridSize);
   cellWidth = width/gridSize;
   cellHeight = height/gridSize;
 }
 
 function draw() {
   background(220);
-  if (autoplay && frameCount % 20 === 0) {
-    nextTurn;
+  if (autoPlay && frameCount % 1 === 0) {
+    nextTurn();
   }
-  
   displayGrid();
 }
 
 function keyPressed() {
   if (key === "e") {
-    grid = createEmpty2DArray(gridSize, gridSize, 0);
-  }
-  if (key === "b") {
-    grid = createEmpty2DArray(gridSize, gridSize, 1);
+    grid = createEmpty2DArray(gridSize, gridSize);
   }
   if (key === "r") {
     grid = createRandom2DArray(gridSize, gridSize);
@@ -35,17 +36,21 @@ function keyPressed() {
     nextTurn();
   }
   if (key === "p") {
-    autoplay = !autoplay;
+    autoPlay = !autoPlay;
+  }
+  if (key === "g") {
+    grid = gun;
   }
 }
 
 function nextTurn() {
-  let newGrid = createEmpty2DArray(gridSize, gridSize);
+  let newBoard = createEmpty2DArray(gridSize, gridSize);
 
   for (let y=0; y<gridSize; y++) {
     for (let x=0; x<gridSize; x++) {
       let neighbours = 0;
 
+      //look at all neighbours and count them
       for (let i=-1; i<=1; i++) {
         for (let j=-1; j<=1; j++) {
           if (y+i>=0 && x+j>=0 && y+i<gridSize && x+j<gridSize) {
@@ -54,28 +59,30 @@ function nextTurn() {
         }
       }
 
+      //don't count yourself
       neighbours -= grid[y][x];
 
-      if (grid[y][x] === 1) {
+      //apply rules of game
+      if (grid[y][x] === 1) { //alive
         if (neighbours === 2 || neighbours === 3) {
-          newGrid[y][x] = 1;
+          newBoard[y][x] = 1;
         }
         else {
-          newGrid[y][x] = 0;
+          newBoard[y][x] = 0;
         }
       }
 
-      if (grid[y][x] === 0) {
+      if (grid[y][x] === 0) { //dead
         if (neighbours === 3) {
-          newGrid[y][x] = 1;
+          newBoard[y][x] = 1;
         }
         else {
-          newGrid[y][x] = 0;
+          newBoard[y][x] = 0;
         }
       }
     }
   }
-  grid = newGrid;
+  grid = newBoard;
 }
 
 function mousePressed() {
@@ -94,39 +101,39 @@ function displayGrid() {
   for (let y=0; y<gridSize; y++) {
     for (let x=0; x<gridSize; x++) {
       if (grid[y][x] === 0) {
-        fill(255);
+        fill("white");
       }
-      else if (grid[y][x] === 1) {
-        fill(0);
+      if (grid[y][x] === 1) {
+        fill("black");
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
 }
 
-function createRandom2DArray(rows, cols) {
-  let grid = [];
+function createEmpty2DArray(rows, cols) {
+  let board = [];
   for (let y=0; y<rows; y++) {
-    grid.push([]);
+    board.push([]);
     for (let x=0; x<cols; x++) {
-      if (random(100) < 50) {
-        grid[y].push(0);
-      }
-      else {
-        grid[y].push(1);
-      }
+      board[y].push(0);
     }
   }
-  return grid;
+  return board;
 }
 
-function createEmpty2DArray(rows, cols, fill) {
-  let grid = [];
+function createRandom2DArray(rows, cols) {
+  let board = [];
   for (let y=0; y<rows; y++) {
-    grid.push([]);
+    board.push([]);
     for (let x=0; x<cols; x++) {
-      grid[y].push(fill);
+      if (random(100) < 50) {
+        board[y].push(0);
+      }
+      else {
+        board[y].push(1);
+      }
     }
   }
-  return grid;
+  return board;
 }
