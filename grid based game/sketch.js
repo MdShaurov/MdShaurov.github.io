be744da0281a67cca36ca7a479542352edfe1003
MdShaurov <shaurovmd123@gmail.com>
2021-10-19 12:13:07 -0600

    Started grid based game

--- grid based game/sketch.js
+++ grid based game/sketch.js
@@ -1,17 +1,34 @@
-// Project Title
-// Your Name
-// Date
+// Grid Based Game
+// Md Shaurov
+// November 7; 2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Global Variables
+let gridSize = 10;
+let grid, cellWidth, cellHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = create2DArray(gridSize, gridSize);
+  cellWidth = width/gridSize;
+  cellHeight = height/gridSize;
 }
 
 function draw() {
   background(220);
   
-  rect(mouseX, mouseY, 100, 50);
+  displayGrid();
+}
+
+function create2DArray(rows, cols) {
+  let grid = [];
+  for (let y=0; y<rows; y++) {
+    grid.push([]);
+    for (let x=0; x<cols; x++) {
+      grid[y].push(0);
+    }
+  }
+  return grid;
 }
