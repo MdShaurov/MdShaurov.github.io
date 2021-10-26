84cbd7088b972bf4248d2747fabce98bbf11bb53
MdShaurov <shaurovmd123@gmail.com>
2021-10-26 12:12:28 -0600

    Update

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -6,111 +6,99 @@
 // - describe what you did to take this project "above and beyond"
 
 // Global Variables
-let gridSize = 10;
-let grid, cellWidth, cellHeight;
-let playerX = 0;
-let playerY = 0;
-let objectX = 0;
-let objectY = 0;
-let loadLevel, sky, coin, empty, grass;
+let grid;
+let BG;
+let grass, coin, mystery, fly, player1, slime, empty;
+let gridHeight, gridWidth;
+let cellWidth, cellHeight;
+let levelToLoad;
+let lines;
 
 function preload() {
-  // Loading level
-  loadLevel = "assets/levels/level1.txt";
-  lines = loadStrings(loadLevel);
+  //load level data
+  levelToLoad = "assets/levels/level1.txt";
+  lines = loadStrings(levelToLoad);
 
-  // Background
-  sky = loadImage("assets/textures/sky/sky.png");
+  //load background
+  BG = loadImage("assets/textures/background/sky.png");
 
-  // Coins
-  coin = loadImage("assets/textures/coin/coinShine/coin1.png");
-
-  // Blocks
+  //load tile images
   grass = loadImage("assets/textures/blocks/grass.png");
+  coin = loadImage("assets/textures/coin/coin.png");
+  mystery = loadImage("assets/textures/blocks/mystery.png");
+  fly = loadImage("assets/textures/mobs/fly.png");
+  player1 = loadImage("assets/textures/player/player1.png");
+  slime = loadImage("assets/textures/mobs/slime.png");
   empty = loadImage("assets/textures/blocks/empty.png");
 }
 
 function setup() {
-  createCanvas(windowHeight*1.2, windowHeight*0.9);
-  grid = createRandom2DArray(gridSize, gridSize);
-  cellWidth = width/gridSize;
-  cellHeight = height/gridSize;
+  // keep this a 4:3 ratio, or it will stretch in weird ways
+  createCanvas(windowWidth, windowHeight);
 
-  // grid[playerY][playerX] = 1;
-}
+  gridHeight = lines.length;
+  gridWidth = lines[0].length;
 
-function draw() {
-  displayGrid();
-}
+  cellWidth = width / gridWidth;
+  cellHeight = height / gridHeight;
 
-function mousePressed() {
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
+  grid = createEmpty2dArray(gridWidth, gridHeight);
 
-  if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
-  }
-  else if (grid[cellY][cellX] === 1 ) {
-    grid[cellY][cellX] = 0;
-  }
-}
-
-
-// function keyPressed() {
-//   if (key === "w") {
-//     playerMove(playerX, playerY-1);
-//   }
-//   else if (key === "a") {
-//     playerMove(playerX-1, playerY);
-//   }
-//   else if (key === "s") {
-//     playerMove(playerX, playerY+1);
-//   }
-//   else if (key === "d") {
-//     playerMove(playerX+1, playerY);
-//   }
-// }
-
-function displayGrid() {
-  image(sky, 0, 0, width, height);
-
-  for (let y=0; y<gridSize; y++) {
-    for (let x=0; x<gridSize; x++) {
-      if (grid[y][x] === 0) {
-        image(empty, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      }
-      if (grid[y][x] === 1) {
-        image(grass, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      }
+  //put values into 2d array of characters
+  for (let y = 0; y < gridHeight; y++) {
+    for (let x = 0; x < gridWidth; x++) {
+      let cellType = lines[y][x];
+      grid[y][x] = cellType;
     }
   }
 }
 
-function readLevel() {
-  if 
+function draw() {
+  display();
 }
-// function playerMove(newX, newY) {
-//   if (newX>=0 && newY>=0 && newX<gridSize && newY<gridSize) {
-//     grid[playerY][playerX] = 0;
 
-//     playerX = newX;
-//     playerY = newY;
+function display() {
+  image(BG, 0, 0, width, height);
 
-//     grid[playerY][playerX] = 1;
-//   }
-// }
+  for (let y = 0; y < gridHeight; y++) {
+    for (let x = 0; x < gridWidth; x++) {
+      showTile(grid[y][x], x, y);
+    }
+  }
+}
 
-function createRandom2DArray(rows, cols) {
+function showTile(location, x, y) {
+  if (location === "#") {
+    image(grass, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+  }
+  else if (location === "C") {
+    image(coin, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+  }
+  else if (location === "B") {
+    image(box, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+  }
+  else if (location === "F") {
+    image(fly, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+  }
+  else if (location === "P") {
+    player[x] = grid[y][x];
+    player[x] = grid[y];
+    image(player1, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+  }
+  else if (location === "S") {
+    image(slime, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+  }
+  else {
+    image(empty, x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+  }
+}
+
+function createEmpty2dArray(cols, rows) {
   let grid = [];
-  for (let y=0; y<rows; y++) {
+  for (let y = 0; y < rows; y++) {
     grid.push([]);
-    for (let x=0; x<cols; x++) {
-      if (random(100) > 50) {
-        grid[y].push(0);
-      }
-      else {
-        grid[y].push(1);
-      }
+    for (let x = 0; x < cols; x++) {
+      grid[y].push(0);
     }
   }
   return grid;
