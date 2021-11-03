1f8ef6bfc8b3bd822989b120e4dd05b49caccebe
MdShaurov <shaurovmd123@gmail.com>
2021-11-03 12:11:35 -0600

    Update

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -8,42 +8,62 @@
 // Global Variables
 let grid, gridWidth, gridHeight;
 let cellWidth, cellHeight;
+let spawnBlock;
 let start, end;
 
 function setup() {
   createCanvas(windowHeight/2, windowHeight);
 
-  gridWidth = 20;
-  gridHeight = 9;
+  gridWidth = 9;
+  gridHeight = 20;
 
-  grid = createEmpty2DArray(gridWidth, gridHeight);
-  cellWidth = (width-0.7)/gridHeight;
-  cellHeight = height/gridWidth;
+  grid = baseGrid(gridWidth, gridHeight);
+  cellWidth = (width-0.7)/gridWidth;
+  cellHeight = height/gridHeight;
 }
 
 function draw() {
   background(220);
   
+
   display();
 }
 
 function display() {
-  for (let y=0; y<gridWidth; y++) {
-    for (let x=0; x<gridHeight; x++) {
-      if (grid[y][x] === 0) {
-        fill()
+  for (let y=0; y<gridHeight; y++) {
+    for (let x=0; x<gridWidth; x++) {
+      if (grid[y][x] === 1) {
+        fill(0);
+        rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+      else {
+        fill(255);
         rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
     }
   }
 }
 
-function createEmpty2DArray(rows, cols) {
+function update() {
+  for (let y=0; y<gridHeight; y++) {
+    for (let x=0; x<gridWidth; x++) {
+      if (grid[y][x] === 1 && y !== 7) {
+        if (grid[y+1][x] !== 1) {
+
+          grid[y][x] = 0;
+          grid[y+1][x] = 1;
+        }
+      }
+    }
+  }
+}
+
+function baseGrid(cols, rows) {
   let grid = [];
   for (let y=0; y<rows; y++) {
     grid.push([]);
     for (let x=0; x<cols; x++) {
-      if (y === 1 && x>=3 && x<=8) {
+      if (y === 1 && x>=3 && x<=5) {
         grid[y].push(1);
       }
       else {
@@ -51,5 +71,6 @@ function createEmpty2DArray(rows, cols) {
       }
     }
   }
+
   return grid;
 }
